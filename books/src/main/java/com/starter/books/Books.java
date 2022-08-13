f34b660c52bb4a307f485ba74701082c68319d90
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2022-08-13 18:39:02 +0600

    Adding books module

---
+++ books/src/main/java/com/starter/books/Books.java
@@ -0,0 +1,11 @@
+package com.starter.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Books  {
+    public static void main(String[] args){
+        SpringApplication.run(Books.class, args);
+    }
+}
--- src/main/java/com/starter/librarian/Library.java
+++ src/main/java/com/starter/librarian/Library.java
@@ -8,6 +8,7 @@ public class Library
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 }
