a05f1ec64e7431019384b337cdf22b2c224496d3
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2022-08-13 18:08:34 +0600

    Initial Commit for Project

---
+++ src/main/java/com/starter/librarian/Library.java
@@ -0,0 +1,13 @@
+package com.starter.librarian;
+
+/**
+ * Hello world!
+ *
+ */
+public class Library
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
